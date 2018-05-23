f5713279f0a2adab33460e29a451e7b56be6647b
Per Johansson <per.johansson@post.com>
2018-05-23 21:47:42 +0100

    setting up stack as official client

--- src/main/java/com/headstartech/jmeter/diameter/DiameterStack.java
+++ src/main/java/com/headstartech/jmeter/diameter/DiameterStack.java
@@ -1,16 +1,21 @@
 package com.headstartech.jmeter.diameter;
 
 import org.jdiameter.api.*;
-import org.jdiameter.client.impl.StackImpl;
-import org.jdiameter.client.impl.helpers.XMLConfiguration;
+import org.jdiameter.server.impl.StackImpl;
+import org.jdiameter.server.impl.helpers.XMLConfiguration;
 import org.mobicents.diameter.dictionary.AvpDictionary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class DiameterStack {
+
+    private static final Logger logger = LoggerFactory.getLogger(DiameterStack.class);
 
     private final Stack stack;
     private final SessionFactory sessionFactory;
@@ -22,6 +27,26 @@ public class DiameterStack {
         AvpDictionary dictionary = AvpDictionary.INSTANCE;
         dictionary.parseDictionary(diameterStackConfiguration.getDictionaryFile().getPath());
         diameterHelper = new DiameterHelper(dictionary);
+
+        stack.getMetaData().getLocalPeer().getCommonApplications().add(diameterStackConfiguration.getApplicationId());
+        Set<ApplicationId> appIds = stack.getMetaData().getLocalPeer().getCommonApplications();
+
+        logger.info("Diameter Stack  :: Supporting " + appIds.size() + " applications.");
+        for (org.jdiameter.api.ApplicationId x : appIds) {
+            logger.info("Diameter Stack  :: Common :: " + x);
+        }
+
+        // Register network req listener, even though we wont receive requests
+        // this has to be done to inform stack that we support application
+        Network network = stack.unwrap(Network.class);
+        network.addNetworkReqListener(new NetworkReqListener() {
+
+            @Override
+            public Answer processRequest(Request request) {
+                //this won't be called.
+                return null;
+            }
+        }, diameterStackConfiguration.getApplicationId()); //passing our example app id.
     }
 
     public void start() throws InternalException, IllegalDiameterStateException {
